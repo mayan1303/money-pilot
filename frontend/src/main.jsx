5e56875eb26c200a09400f46ed214616cb65b188
mayan1303 <jindalmayan1303@gmail.com>
2026-04-14 05:41:13 +0530

    fix router issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// 👇 IMPORT CONTEXT
+import { BrowserRouter } from "react-router-dom";   // ✅ ADD THIS
 import { AppProvider } from "./context/AppContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <BrowserRouter>      {/* ✅ ADD THIS */}
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
